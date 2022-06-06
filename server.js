bca0f2045a21ca7729ece83f2d8e7a0dba5930d2
Roberto Guevara <robertoeguevara@gmail.com>
2022-06-05 23:53:50 -0700

    creat app.js and test server works

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const http = require('http');
+const app = require('./app.js');
 
 // Environment variable or default port 3000
 const port = process.env.PORT || 3000;
 
-const server = http.createServer();
+const server = http.createServer(app);
 
 server.listen(port);
