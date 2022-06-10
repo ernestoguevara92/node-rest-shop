91fd55ef946849169bcce69263fa503fb8e42a45
Roberto Guevara <robertoeguevara@gmail.com>
2022-06-10 14:30:13 -0700

    upload image with filter to accept jpg and png format only

--- api/routes/products.js
+++ api/routes/products.js
@@ -3,22 +3,37 @@ const router = express.Router();
 const mongoose = require('mongoose');
 const multer = require('multer');
 
-const storage = multer.diskStorage({
+const storageFolder = multer.diskStorage({
     destination: function(req, file, cb) {
         cb(null, './uploads/');
     },
     filename: function(req, file, cb) {
-        cb(null, new Date().toISOString() + file.originalname);
+        cb(null, new Date().toISOString().replace(/:/g, '-') + file.originalname);
     }
 });
 
-const upload = multer({storage: storage});
+const fileFilter = (req, file, cb) => {
+    // reject a file
+    if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
+        cb(null, true);
+    } else {
+        cb(null, false);
+    }
+};
+
+const upload = multer({
+    storage: storageFolder, 
+    limits: {
+        fileSize: 1024 * 1024 *5
+    },
+    fileFilter: fileFilter
+});
 
 const Product = require('../models/products');
 // query to Database
 router.get('/', (req, res, next) => {
     Product.find()
-    .select('name price _id')
+    .select('name price _id productImage')
     .exec()
     .then(docs => {
         const response = {
@@ -27,6 +42,7 @@ router.get('/', (req, res, next) => {
                 return {
                     name: doc.name,
                     price: doc.price,
+                    productImage: doc.productImage,
                     _id: doc._id,
                     request: {
                         type: 'GET',
@@ -55,7 +71,8 @@ router.post('/', upload.single('productImage'), (req, res, next) => {
     const product = new Product({
        _id: new mongoose.Types.ObjectId(),
        name: req.body.name,
-       price: req.body.price
+       price: req.body.price,
+       productImage: req.file.path
     });
 
     product
@@ -86,7 +103,7 @@ router.post('/', upload.single('productImage'), (req, res, next) => {
 router.get('/:productId', (req, res, next) => {
     const id = req.params.productId;
     Product.findById(id)
-        .select('name price _id')
+        .select('name price _id productImage')
         .exec()
         .then(doc => {
             console.log("From database", doc);
@@ -155,6 +172,5 @@ router.delete('/:productId', (req, res, next) => {
        });
    });
 });
-
 
 module.exports = router;
--- app.js
+++ app.js
@@ -15,6 +15,7 @@ mongoose.connect('mongodb+srv://roberto:' + process.env.MONGO_ATLAS_PW + '@clust
 mongoose.Promise = global.Promise;
 
 app.use(morgan('dev'));
+app.use('/uploads', express.static('uploads'));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
