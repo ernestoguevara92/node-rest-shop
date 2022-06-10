63952ece465b07b90bad282dc7196b05c2f8eb33
Roberto Guevara <robertoeguevara@gmail.com>
2022-06-10 12:48:13 -0700

    add uploads folder for images

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,6 +1,18 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
+const multer = require('multer');
+
+const storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, './uploads/');
+    },
+    filename: function(req, file, cb) {
+        cb(null, new Date().toISOString() + file.originalname);
+    }
+});
+
+const upload = multer({storage: storage});
 
 const Product = require('../models/products');
 // query to Database
@@ -39,7 +51,7 @@ router.get('/', (req, res, next) => {
     });
 });
 
-router.post('/', (req, res, next) => {
+router.post('/', upload.single('productImage'), (req, res, next) => {
     const product = new Product({
        _id: new mongoose.Types.ObjectId(),
        name: req.body.name,
