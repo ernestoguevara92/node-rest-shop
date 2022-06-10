e288fe304312ff59505d7f72ef5b0d704eb20959
Roberto Guevara <robertoeguevara@gmail.com>
2022-06-10 00:08:56 -0700

    validate data and create orders schema

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ mongoose.connect('mongodb+srv://roberto:' + process.env.MONGO_ATLAS_PW + '@clust
         useMongoClient: true
     }
 );
+mongoose.Promise = global.Promise;
 
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended: false}));
