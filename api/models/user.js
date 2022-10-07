9231878065d2125f9aac4894c351a174fd9d41bd
Roberto Guevara <robertoeguevara@gmail.com>
2022-10-06 21:15:23 -0700

    modify schemas

--- api/models/user.js
+++ api/models/user.js
@@ -8,7 +8,10 @@ const userSchema = mongoose.Schema({
         unique: true, 
         match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
     },
-    password: { type: String, required: true }
+    password: { type: String, required: true },
+    username: { type: String, required: true, unique: true },
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
 });
 
 module.exports = mongoose.model('User', userSchema);
